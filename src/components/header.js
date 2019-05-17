eb3afdc202ac0fc553e242fcfbdc1ec75fac6044
Yaroslav <doctor355@yahoo.com>
2019-05-17 18:05:32 +0300

    Layout with min styles

--- src/components/header.js
+++ src/components/header.js
@@ -1,27 +1,57 @@
 import React from 'react';
 import {Link} from 'gatsby';
+// import './header.module.scss';
+import myHeaderStyles from "./header.module.scss";
 
 const Header = () => {
   return (
-    <div>
-      <h1>Header title</h1>
+    <header className={myHeaderStyles.header}>
+      <h1>
+        <Link className={myHeaderStyles.title} to="/">
+          Header title
+        </Link>
+      </h1>
       <nav>
-        <ul>
+        <ul className={myHeaderStyles.navList}>
           <li>
-            <Link to="/">Home</Link>
+            <Link
+              className={myHeaderStyles.navItem}
+              activeClassName={myHeaderStyles.activeNavItem}
+              to="/"
+            >
+              Home
+            </Link>
           </li>
           <li>
-            <Link to="/about">About</Link>
+            <Link
+              className={myHeaderStyles.navItem}
+              activeClassName={myHeaderStyles.activeNavItem}
+              to="/about"
+            >
+              About
+            </Link>
           </li>
           <li>
-            <Link to="/blog">Blog</Link>
+            <Link
+              className={myHeaderStyles.navItem}
+              activeClassName={myHeaderStyles.activeNavItem}
+              to="/blog"
+            >
+              Blog
+            </Link>
           </li>
           <li>
-            <Link to="/contact">Contact</Link>
+            <Link
+              className={myHeaderStyles.navItem}
+              activeClassName={myHeaderStyles.activeNavItem}
+              to="/contact"
+            >
+              Contact
+            </Link>
           </li>
         </ul>
       </nav>
-    </div>
+    </header>
   )
 };
 
--- src/components/layout.js
+++ src/components/layout.js
@@ -3,15 +3,18 @@ import React from 'react';
 import Footer from "../components/footer";
 import Header from "../components/header";
 import '../styles/index.scss';
+import LayoutStyles from './layout.module.scss';
 
 const Layout = (props) => {
   return (
-    <div>
-      <Header />
-      {props.children}
+    <div className={LayoutStyles.container}>
+      <div className={LayoutStyles.content}>
+        <Header />
+        {props.children}
+      </div>
       <Footer />
     </div>
-  );
+  )
 };
 
 export default Layout;
