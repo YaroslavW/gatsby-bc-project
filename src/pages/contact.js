cde3f6fc2cb5d5a6ff1a5143a2ffadb02a6265d7
Yaroslav <doctor355@yahoo.com>
2019-05-17 16:21:35 +0300

    Change version tar for warning gitHub delete

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -3,11 +3,19 @@ import Layout from "../components/layout";
 
 const ContactPage = () => {
   return (
-    <Layout>     
+    <Layout>
       <h1>Contact</h1>
       <p>
-        The best way to find me its visiting on 
-        <a href="https://github.com/YaroslavW" target="_blank" rel="nofollow noreferrer" title="My gitHub-page"> my page</a>
+        The best way to find me its visiting on
+        <a
+          href="https://github.com/YaroslavW"
+          target="_blank"
+          rel="noopener noreferrer"
+          title="My gitHub-page"
+        >
+          {" "}
+          my page
+        </a>
       </p>
     </Layout>
   )
