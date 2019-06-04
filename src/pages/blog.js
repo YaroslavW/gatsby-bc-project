f9b72c6ba90395f42d28e294aaefabe107ac0ab9
Yaroslav <doctor355@yahoo.com>
2019-06-04 19:05:16 +0300

    Output All Posts

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,13 +1,40 @@
 import React from 'react';
+import {graphql, useStaticQuery} from 'gatsby';
 import Layout from "../components/layout"
 
 const BlogPage = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      allMarkdownRemark {
+        edges {
+          node {
+            frontmatter {
+              title
+              data
+            }
+          }
+        }
+      }
+    }
+  `)
+
+  // console.log(data)
+
   return (
-    <Layout>     
+    <Layout>
       <h1>Blog</h1>
-      <p>Posts will show up here leter on...</p> 
+      <ol>
+        {data.allMarkdownRemark.edges.map(edge => {
+          return (
+            <li>
+              <h1>{edge.node.frontmatter.title}</h1>
+              <p>{edge.node.frontmatter.data}</p>
+            </li>
+          )
+        })}
+      </ol>
     </Layout>
-  );
+  )
 };
 
 export default BlogPage;
